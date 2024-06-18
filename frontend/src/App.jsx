e2f09e5d24ff1154d3e8fb03898db59197a89d85
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-06-18 20:07:12 +0530

    added dashboard and success page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
-// import { Dashboard } from "./pages/Dashboard";
-// import { SendMoney } from "./pages/SendMoney";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
@@ -11,11 +11,10 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
-      <h1>hi</h1>
     </>
   );
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,15 @@
-function Dashboard() {
-  return <div>dash</div>;
-}
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
 
-export default Dashboard;
+export const Dashboard = () => {
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={"10,000"} />
+        <Users />
+      </div>
+    </div>
+  );
+};

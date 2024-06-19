23b850a761b886b8b1d46107dd6ac44ed32a1eff
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-06-20 03:21:55 +0530

    added sendmoney feature

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Button } from "./Button";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Users = () => {
   const [users, setUsers] = useState([]);
@@ -37,6 +38,7 @@ export const Users = () => {
 };
 
 function User({ user }) {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between">
       <div className="flex">
@@ -53,7 +55,12 @@ function User({ user }) {
       </div>
 
       <div className="flex flex-col justify-center h-ful">
-        <Button label={"Send Money"} />
+        <Button
+          onClick={(e) => {
+            navigate("/send?id=" + user._id + "&name=" + user.firstName);
+          }}
+          label={"Send Money"}
+        />
       </div>
     </div>
   );
